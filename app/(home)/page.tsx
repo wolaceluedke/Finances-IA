a59aff68526d7bc6795b989742428492ca5fcd67
Wolace Luedke <wolacepin@hotmail.com>
2025-01-09 00:01:48 -0300

    feat: ensure get dashboard function is protected

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,7 +22,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   }
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(`?month=${new Date().getMonth() + 1}`);
   }
   const dashboard = await getDashboard(month);
   return (
--- app/_data/getDashboard/index.ts
+++ app/_data/getDashboard/index.ts
@@ -1,8 +1,15 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
 import { TotalExpensePerCategory, TransactionPercentagePerType } from "./types";
+import { auth } from "@clerk/nextjs/server";
+
 export const getDashboard = async (month: string) => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
   const where = {
+    userId,
     date: {
       gte: new Date(`2024-${month}-01`),
       lt: new Date(`2024-${month}-31`),
